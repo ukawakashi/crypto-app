fbce199e3c4ee858aaf3b9fbdd25095e22500e7c
PTChinh <44965578+PTChinh@users.noreply.github.com>
2021-10-14 23:13:27 +0700

    Handle redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from "./components";
-import {Layout} from "antd";
+import {Layout, Space, Typography} from "antd";
 
 import "./App.css";
-import {Route, Switch} from "react-router-dom";
+import {Link, Route, Switch} from "react-router-dom";
 
 const App = () => {
     return (
@@ -33,9 +33,17 @@ const App = () => {
                         </Switch>
                     </div>
                 </Layout>
-            </div>
-            <div className="footer">
-
+                <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br/>
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '0b18c91c14mshc7e64dc18c02f22p197215jsnd0e8c51222df'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
