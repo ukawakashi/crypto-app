e90287737e2790d1a55ea7bb526a38d40444a6e5
PTChinh <44965578+PTChinh@users.noreply.github.com>
2021-10-14 23:13:57 +0700

    Handle CryptocurrenciesPage

---
+++ src/components/Cryptocurrencies.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from 'react';
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const Cryptocurrencies = ({simplified}) => {
+
+    const count = simplified ? 10 : 100;
+
+    const  {data: cryptosList, isFetching} = useGetCryptosQuery(count);
+
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+    const [searchItem, setSearchItem] = useState('');
+
+    useEffect(() => {
+        const filterData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchItem));
+        setCryptos(filterData);
+    }, [cryptosList, searchItem]);
+
+    if (isFetching) return 'Loading...';
+
+    return (
+        <>
+            {!simplified && (
+                <div className="search-crypto">
+                    <Input placeholder="Search Cryptocurrency" onChange={(event) => setSearchItem(event.target.value)}/>
+                </div>
+            )}
+            <Row gutter={[32, 32]} className="crypto-card-container">
+                {
+                    cryptos?.map((currency) => (
+                        <Col xs={24} lg={6} sm={12} className="crypto-card" key={currency.id}>
+                            <Link to={`/crypto/${currency.id}`}>
+                                <Card
+                                    title={`${currency.rank}. ${currency.name}/${currency.symbol}`}
+                                    extra={<img src={currency.iconUrl} className="crypto-image" alt=""/>}
+                                    hoverable
+                                >
+                                    <p>Price: {millify(currency.price)}</p>
+                                    <p>Market Cap: {millify(currency.marketCap)}</p>
+                                    <p>Daily Change: {millify(currency.change)}%</p>
+                                </Card>
+                            </Link>
+                        </Col>
+                    ))
+                }
+            </Row>
+        </>
+    );
+};
+
+export default Cryptocurrencies;
